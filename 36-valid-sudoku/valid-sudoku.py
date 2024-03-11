f14d069d6861c51082518ee36a912e2a798fac88
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-03-11 23:32:03 +1100

    Time: 98 ms (26.63%) | Memory: 16.5 MB (69.62%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -2,26 +2,20 @@ import collections
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        #use a dict + set to keep a list of values at each row, col and square
-        #example: {0: (1,2,3,4), 1: (1,2,3,4)}
         rows = collections.defaultdict(set)
         cols = collections.defaultdict(set)
-        square = collections.defaultdict(set)
+        grid = collections.defaultdict(set)
 
         for r in range(9):
           for c in range(9):
-            if board[r][c] == '.':
+            if board[r][c] == ".":
               continue
-            elif (
-            #check if val in a particular row set
-            board[r][c] in rows[r]
-            #check if val in a particular col set
-            or board[r][c] in cols[c]
-            #check if val in a particular square set 
-            or board[r][c] in square[(r // 3 , c // 3)]):
+            elif board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in grid[r // 3, c // 3]:
               return False
+            
             else:
               rows[r].add(board[r][c])
               cols[c].add(board[r][c])
-              square[(r // 3, c //3)].add(board[r][c])
+              grid[r//3, c//3].add(board[r][c])
         return True
+
