feb38d74e8e630fb588fb25cba7bd5f719dae832
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-04-04 22:44:40 +1100

    Time: 78 ms (17.04%) | Memory: 16.7 MB (11.09%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        combinations = []
+        self.helper(0, [], combinations, candidates, target, 0)
+        return combinations
+
+    def helper(self, i, curSet, combinations, candidates, target, total):
+      if total == target:
+        combinations.append(curSet.copy())
+        return
+
+      elif i == len(candidates) or total > target:
+        return
+      
+      curSet.append(candidates[i])
+      self.helper(i + 1, curSet, combinations, candidates, target, total + candidates[i])
+      
+      curSet.pop()
+
+      while i + 1 < len(candidates) and candidates[i] == candidates[i+1]:
+        i += 1
+      self.helper(i + 1, curSet, combinations, candidates, target, total)
+
