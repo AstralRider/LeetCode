dea19c9ed1a895d45458b0fb26fe4d3a715f99ef
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-04-07 22:13:08 +1000

    Time: 467 ms (56.81%) | Memory: 35.3 MB (40.55%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,33 +1,30 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-      res = []
-      string = []
-      def dfs(index):
-        if index >= len(s):
-          res.append(string.copy())
-          return
-
-        for j in range(index, len(s)):
-          if self.isPalindrome(s, index, j):
-            string.append(s[index : j+1])
-            dfs(j + 1)
-            string.pop()
-        
-      dfs(0)
-      return res
-        
-    def isPalindrome(self, s, start, end):
-      L = start
-      R = end
-      
-      while L < R:
-        if s[L] == s[R]:
-          L += 1
-          R -=1
-        else:
-          return False
-      return True
-
+        res = []
+        curr = []
+        def dfs(i):
+            if i >= len(s):
+                res.append(curr.copy())
+                return
           
+            for j in range(i, len(s)):
+                if self.isPal(i, j, s):
+                    curr.append(s[i: j + 1])
+                    dfs(j + 1)
+                    curr.pop()
+        
+        dfs(0)
+        return res
+    
+    def isPal(self, L, R, string):
 
+        while L <= R:
+            if string[L] != string[R]:
+                return False
+            else:
+                L += 1
+                R -= 1
+        return True
+    
           
+    
