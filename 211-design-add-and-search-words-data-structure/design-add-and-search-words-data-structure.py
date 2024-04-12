c904ac1470ab41362bbb229b690712f7142eeeaa
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-04-12 19:29:53 +1000

    Time: 1660 ms (35.72%) | Memory: 64.3 MB (43.76%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -0,0 +1,48 @@
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.word = True
+        
+    def search(self, word: str) -> bool:
+        
+
+        def dfs(i, node):
+            curr = node
+            
+            if i == len(word):
+                return curr.word
+            
+            for j in range(i, len(word)):
+                if word[j] == ".":
+                    for n in curr.children.values():
+                        if dfs(j + 1, n):
+                            return True
+                    return False
+                else:
+                    if word[j] not in curr.children:
+                        return False
+                    return dfs(j+1, curr.children[word[j]])
+            return curr.word
+        
+        return dfs(0, self.root)
+
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
