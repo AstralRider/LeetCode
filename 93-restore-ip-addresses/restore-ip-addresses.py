acae5e16a282d7b0f34f74d8e217b612e0e7346b
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-04-13 08:59:31 +1000

    Time: 32 ms (90.56%) | Memory: 16.5 MB (55.44%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -0,0 +1,27 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res = []
+
+        def dfs(i, dots, string):
+            if i >= len(s) and dots == 4:
+                res.append(string[:-1])
+                return
+            if dots >= 4:
+                return
+            
+            for j in range(i,  len(s)):
+                
+                digits = s[i: j+1]
+
+                if len(digits) > 1 and digits[0] == '0':
+                    return False
+
+                if len(digits) > 3 or int(digits) > 255:
+                    return False
+                
+                dfs(j+1, dots +1 , string + digits + ".")
+
+        dfs(0, 0, "")
+        return res
+
+
