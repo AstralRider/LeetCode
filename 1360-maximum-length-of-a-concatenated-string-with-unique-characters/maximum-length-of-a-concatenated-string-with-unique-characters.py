d4ff0723abbc762daaefce02fbe0152cf52aa10a
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-04-19 08:04:22 +1000

    Time: 1135 ms (5.13%) | Memory: 16.6 MB (81.72%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -3,9 +3,10 @@ class Solution:
         self.length = 0
 
         def backtrack(i, string):
+            
+            if self.checkUnique(string):
+                self.length = max(self.length, len(string))
             if i >= len(arr):
-                if self.checkUnique(string):
-                    self.length = max(self.length, len(string))
                 return
 
             backtrack(i + 1, string + arr[i])
