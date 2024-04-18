86835617d8735937e191697c718cfcab153a1c0d
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-04-19 08:25:30 +1000

    Time: 84 ms (61.66%) | Memory: 16.6 MB (81.72%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,19 +1,22 @@
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
         self.length = 0
-
-        def backtrack(i, string):
-            if i >= len(arr):
-                if len(string) == len(set(string)):
-                    self.length = max(self.length, len(string))
+        
+        def backtrack(i, chars):
+            if i == len(arr):
+                self.length = max(self.length, len(chars))
                 return
             
-            backtrack(i + 1, string + arr[i])
-
-            backtrack(i + 1, string)
+            if len(set(arr[i])) == len(arr[i]):  # Check if current string has no duplicates
+                for c in arr[i]:
+                    if c in chars:
+                        break
+                else:
+                    backtrack(i + 1, chars | set(arr[i]))
+            
+            backtrack(i + 1, chars)
         
-        backtrack(0, "")
-
+        backtrack(0, set())
         return self.length
     
     def checkUnique(self, string):
