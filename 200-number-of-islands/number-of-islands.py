5d1a4f632dea89e5d68811f359cba15cd9c14650
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-04-23 08:19:07 +1000

    Time: 256 ms (38.89%) | Memory: 23.9 MB (35.11%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,34 +1,39 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
+        #dfs or bfs
+        # use a set to track visited 1's
+        # if it is a 1 we haven't seen before, increment islands by 1
         ROWS = len(grid)
         COLS = len(grid[0])
         visited = set()
-        islands = 0
 
-        def bfs(r, c):
-            q = collections.deque()
+        numberOfIslands = 0
+
+        def dfs(r,c,visited):
+            if (
+                r < 0
+                or c < 0
+                or r >= ROWS
+                or c >= COLS
+                or grid[r][c] == "0"
+                or (r,c) in visited):
+                    return
+            
             visited.add((r,c))
-            q.append((r,c))
 
-            while q:
-                r,c = q.popleft()
-                directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
-                
-                #look left, right, up and down for the current node
-                for dr, dc in directions:
-                    row = r + dr
-                    col = c + dc
-                    #check in range, node == 1 and not already visited
-                    if (row in range(ROWS) and col in range(COLS) and grid[row][col] == "1" and (row,col) not in visited):
-                        q.append((row,col))
-                        visited.add((row,col))
+            dfs(r+1,c, visited)
+            dfs(r-1,c, visited)
+            dfs(r,c+1, visited)
+            dfs(r,c-1, visited)
 
         for r in range(ROWS):
-            for c in range (COLS):
-                if grid[r][c] == "1" and (r,c) not in visited:
-                    bfs(r, c)
-                    islands += 1
-        return islands
+            for c in range(COLS):
+                if (r,c) not in visited and grid[r][c] == "1":
+                    dfs(r,c,visited)
+                    numberOfIslands += 1
 
-        
+        return numberOfIslands
+
+
+
