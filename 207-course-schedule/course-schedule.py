4bb444c753b2b17da86ddfe71379fb444a0d6d0e
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-04-26 19:51:46 +1000

    Time: 94 ms (34.32%) | Memory: 18.7 MB (11.91%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,48 +1,43 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        #build adjacency list
         adjList = {}
-        visitSet = set()
-
-        #build an adjacency list
-        for course, prereq in prerequisites:
+        for course, pre in prerequisites:
             if course not in adjList:
                 adjList[course] = []
-            if prereq not in adjList:
-                adjList[prereq] = []
-            adjList[course].append(prereq)
+            if pre not in adjList:
+                adjList[pre] = []
+            adjList[course].append(pre)
         
-        #define our dfs function 
+        #create a set to detect cycles
+        visited = set()
+
         def dfs(course):
-
-            #if our course is in our set, we have a cycle
-            if course in visitSet:
+            if course in visited:
                 return False
-
-            #if our course prereq's list is empty we can complete this course
+            #we have already checked this course and it can be completed
             if len(adjList[course]) == 0:
                 return True
             
-            visitSet.add(course)
-
-            for prereqs in adjList[course]:
-                if not dfs(prereqs):
+            #add course to set of courses we are currently checking
+            visited.add(course)
+            
+            #check if all the prerequisite courses can be completed
+            for prereq in adjList[course]:
+                if not dfs(prereq):
                     return False
-
-            #we have explored this courses is preqreq's and are no longer visiting this course
-            visitSet.remove(course)
-            #we can confirm this course and all its preqreq's can be completed.
+            
+            #remove course from the set since we have finished checking it can be done
+            visited.remove(course)
+            #set the course prerequisites to empty since we know this course can be completed
             adjList[course] = []
-
-            #we can return true from this course
             return True
         
+        #run the dfs for every edge since it is possible we have a disconnected graph
+        #e.g 0 -> 1, 2 -> 3
         for course, pre in prerequisites:
             if not dfs(course):
                 return False
         return True
-                
-
-
-
-        
 
