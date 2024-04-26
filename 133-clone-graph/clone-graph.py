cca2dc1f99677ce4ed4f77cc277da4c2d1298192
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-04-26 23:18:19 +1000

    Time: 33 ms (93.56%) | Memory: 17 MB (9.10%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,22 +9,26 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        
         cloneMap = {}
-
         
         def dfs(node):
             if node in cloneMap:
                 return cloneMap[node]
-            
+
             clone = Node(node.val)
+
             cloneMap[node] = clone
 
             for neighbor in node.neighbors:
                 clone.neighbors.append(dfs(neighbor))
             
             return clone
-        
+
+
         if node:
             return dfs(node)
         else:
-            None
+            return node
+
+        
