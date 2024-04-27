466a37a86792cb4591ba530aecc425d94f225463
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-04-27 11:48:11 +1000

    Time: 89 ms (62.01%) | Memory: 18.9 MB (5.35%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -10,32 +10,25 @@ class Solution:
                 adjList[pre] = []
             adjList[course].append(pre)
         
-        #create a set to detect cycles
         visited = set()
+        path = set()
 
         def dfs(course):
-            if course in visited:
+            if course in path:
                 return False
-            #we have already checked this course and it can be completed
-            if len(adjList[course]) == 0:
+            if course in visited:
                 return True
             
-            #add course to set of courses we are currently checking
-            visited.add(course)
+            path.add(course)
             
-            #check if all the prerequisite courses can be completed
             for prereq in adjList[course]:
                 if not dfs(prereq):
                     return False
             
-            #remove course from the set since we have finished checking it can be done
-            visited.remove(course)
-            #set the course prerequisites to empty since we know this course can be completed
-            adjList[course] = []
+            path.remove(course)
+            visited.add(course)
             return True
-        
-        #run the dfs for every edge since it is possible we have a disconnected graph
-        #e.g 0 -> 1, 2 -> 3
+
         for course, pre in prerequisites:
             if not dfs(course):
                 return False
