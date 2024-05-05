e0c26897c36ba7da40f6488fc4cb2cb1a50bd642
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-05-05 12:08:32 +1000

    Time: 75 ms (60.69%) | Memory: 17 MB (33.91%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/reconstruct-itinerary.py
@@ -1,60 +1,33 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        adj = {src: [] for src, dst in tickets}
+        
+        adjList = {}
+
         res = []
 
-        for src, dst in tickets:
-            adj[src].append(dst)
+        
 
-        for key in adj:
-            adj[key].sort()
+        for src, dest in tickets:
+            if src not in adjList:
+                adjList[src] = []
+            adjList[src].append(dest)
+        
+        for key in adjList.keys():
+            adjList[key].sort(reverse=True)
 
         def dfs(src):
-            if src in adj:
-                destinations = adj[src][:]
-                while destinations:
-                    dest = destinations[0]
-                    adj[src].pop(0)
-                    dfs(dest)
-                    destinations = adj[src][:]
+            if src in adjList:
+                while adjList[src]:
+                    dfs(adjList[src].pop())
+            #an airport has no outgoing flights
             res.append(src)
-
         dfs("JFK")
-        res.reverse()
+        return res[::-1]
 
-        return res
-
-#Code works fine just not on leetcode due to TLE
-# adjList = {}
-
-#         res = ["JFK"]
-
-#         tickets.sort()
-
-#         for src, dest in tickets:
-#             if src not in adjList:
-#                 adjList[src] = []
-#             if dest not in adjList:
-#                 adjList[dest] = []
-#             adjList[src].append(dest)
-
-#         def dfs(src):
-#             if len(res) == len(tickets) + 1:
-#                 return True
-            
-#             if src not in adjList:
-#                 return False
-            
-#             for i, v in enumerate(adjList[src]):
-#                 if v == -1:
-#                     continue
-#                 adjList[src][i] = -1
-#                 res.append(v)
-#                 if dfs(v):
-#                     return True
-#                 adjList[src][i] = v
-#                 res.pop()
-#             return False
         
-#         dfs("JFK")
-#         return res
+            
+        
+       
+
+
+        
