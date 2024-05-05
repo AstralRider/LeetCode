9340caa2d5eb20a59bac6ac9a12c97b8ab3f9fce
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-05-05 14:53:04 +1000

    Time: 114 ms (5.10%) | Memory: 17.1 MB (17.51%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/reconstruct-itinerary.py
@@ -5,8 +5,6 @@ class Solution:
 
         res = []
 
-        
-
         for src, dest in tickets:
             if src not in adjList:
                 adjList[src] = []
@@ -14,18 +12,29 @@ class Solution:
         
         for key in adjList.keys():
             adjList[key].sort(reverse=True)
-
+        
+        #Eulerian Path/Circuit algorithm (Hierholzer's algorithm) 
+        #https://www.youtube.com/watch?v=8MpoO2zA2l4&t=658s&ab_channel=WilliamFiset
+        #Basic overview of the algo is to dfs every vertex on the way down
+        #When we encounter a vertex with no edges, we have found the end
+        #We pop back up the recursive stack to check the edges of other vertices.
+        #we continue doing this until we have visited every edge
         def dfs(src):
             if src in adjList:
                 temp = adjList[src][:]
                 while temp and adjList[src]:
                     val = adjList[src].pop()
                     dfs(val)
-            #an airport has no outgoing flights
+            print(res)
             res.append(src)
         dfs("JFK")
-        return res[::-1]
 
+        return res[::-1]
+#Time:
+#O(E) for Hierholzer's algorithm + O(E*logE) for sorting every edge
+
+#Space
+#O(V + E) for the adjacency list
         
             
         
