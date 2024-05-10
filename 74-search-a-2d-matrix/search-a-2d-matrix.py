27675dcccbad2fa021044be118a02fb1d4c9fa55
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-05-10 19:23:19 +1000

    Time: 50 ms (24.73%) | Memory: 17.1 MB (31.89%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,37 +1,35 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROWS = len(matrix)
-        COLS = len(matrix[0])
+        top = 0 
+        bottom = len(matrix) - 1
 
-        top = 0
-        bottom = ROWS - 1
-        #perform binary search on nested arrays to find the array that may contain target
-        #1.check if target > last index in middle array
-        #2.check if target < first index in middle array
-        #3.adjust array being searched accordingly
         while top <= bottom:
-          row = (top + bottom) // 2
-          if target > matrix[row][-1]:
-            top = row + 1
-          elif target < matrix[row][0]:
-            bottom = row - 1
-          else:
-            break
-
-        #can have a case where top > bottom
-        if not top <= bottom:
-          return False
-        #regular array binary search
-        else:
-          left = 0
-          right = COLS - 1
-          while left <= right:
-            mid = (left + right) // 2
-            if target > matrix[row][mid]:
-              left = mid + 1
-            elif target < matrix[row][mid]:
-              right = mid - 1
+            mid = (top + bottom) // 2
+            if target > matrix[mid][-1]:
+                top = mid + 1
+            elif target < matrix[mid][0]:
+                bottom = mid - 1
             else:
-              return True
-          return False
-            
+                break
+
+
+        l = 0
+        r = len(matrix[mid]) - 1
+        arr = matrix[mid]
+
+        while l <= r:
+            m = (l + r) // 2
+            if target < arr[m]:
+                r = m - 1
+            elif target > arr[m]:
+                l = m + 1
+            else:
+                return True
+        return False
+        
+
+
+
+
+
+
