2b5762731a52589c3675ec68b43d945e02d6a799
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-05-18 11:53:59 +1000

    Time: 54 ms (33.94%) | Memory: 16.7 MB (25.02%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,51 +1,58 @@
+from collections import deque
+
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-       
-       
-        
         ROWS = len(grid)
         COLS = len(grid[0])
-        queue = collections.deque()
+
+        visited = set()
+        orangesCount = 0
+        minutes = 0
+        queue = deque()
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+                    visited.add((i, j))
+                if grid[i][j] == 1:
+                    orangesCount += 1
+        
+        if orangesCount == 0:
+            return 0
         
 
-        freshOrangeCount = 0
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                #find rotten oranges, add it a queue to perform multi-source bfs
-                if grid[r][c] == 2:
-                    queue.append((r,c))
-                if grid[r][c] == 1:
-                    freshOrangeCount += 1
-        
-        minute = 0
-
-        while queue and freshOrangeCount > 0:
-             
+        while queue:
+            if orangesCount > 0:
+                minutes += 1
+                        
             for i in range(len(queue)):
-                r,c = queue.popleft()
+                r, c = queue.popleft()
 
-                directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
                 for dr, dc in directions:
-                    new_row = dr + r
-                    new_col = dc + c
-
-                    if(
-                        new_row in range(ROWS)
-                        and new_col  in range(COLS)
-                        and grid[new_row][new_col] == 1):
-                            queue.append((new_row, new_col))
-                            freshOrangeCount -= 1
-                            grid[new_row][new_col] = 2
-                    
-            minute += 1
-               
-        if freshOrangeCount == 0:
-            return minute
+                    new_r = r + dr
+                    new_c = c + dc
+                    if (
+                        min(new_r, new_c) < 0
+                        or new_r == ROWS
+                        or new_c == COLS
+                        or (new_r, new_c) in visited
+                        or grid[new_r][new_c] == 0
+                    ):
+                        continue
+                    else:
+                        visited.add((new_r, new_c))
+                        queue.append((new_r, new_c))
+                        orangesCount -= 1
+            
+            
+        
+        if orangesCount > 0:
+            return -1 
         else:
-            return -1
-                
-
-
+            return minutes
+        
 
