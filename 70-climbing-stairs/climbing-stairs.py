653a23c35468ba5317c593a7d0ef3b58ee93d560
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-05-26 16:44:33 +1000

    Time: 52 ms (5.52%) | Memory: 16.5 MB (30.34%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,18 +2,15 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         dp = [1, 2]
 
-        if n <= 3:
+        if n <= 2:
             return n
 
-        i = 2
-
-        for idx in range(1, n-1):
+        for idx in range(2, n):
             tmp = dp[1]
             dp[1] = tmp + dp[0]
             dp[0] = tmp
         
         return dp[1]
 
-        #[1, 2]
-        #[2, 3]
-        #[3, 5]
+        #2 [2, 3]
+        #3 [3, 5]
