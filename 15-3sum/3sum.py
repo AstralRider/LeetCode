cde95451cfac337657a823a2d86eb51197960422
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-05-28 22:04:58 +1000

    Time: 837 ms (36.47%) | Memory: 20.5 MB (77.90%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,33 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        #sort array first
-        nums.sort()
+        #
+        #[-4, -1, -1, 0, 1, 2]
         res = []
-
+        #nlogn
+        nums.sort()
         for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            l = i + 1
+            r = len(nums) - 1
 
-          #check for a duplicate value
-          if i > 0 and nums[i] == nums[i-1]:
-            continue
-
-          #left and right pointers
-          L = i + 1
-          R = len(nums) - 1
-
-          #basically 2sum sorted
-          while L < R:
-            threeSum = nums[i] + nums[L] + nums[R]
-
-            if threeSum > 0:
-              R -= 1
-            elif threeSum < 0:
-              L += 1
-            else:
-              res.append([nums[i], nums[L], nums[R]])
-              L += 1
-              R -= 1
-              #handle duplicate Left values to ensure we don't duplicate answers
-              while nums[L] == nums[L - 1] and L < R:
-                L += 1
+            while l < r and l < len(nums):
+                total = nums[i] + nums[l] + nums[r]
+                if total > 0:
+                    r -= 1
+                elif total < 0:
+                    l += 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1 
+                    r -= 1
+                    while nums[l] == nums[l-1] and nums[r] == nums[r+1] and l<r:
+                        l += 1
         return res
+
