e8f613b530eeec62c67b0be30b0af0decb0d73d8
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-05-31 22:26:37 +1000

    Time: 35 ms (63.05%) | Memory: 16.4 MB (98.82%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,12 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        h1 = 0
-        h2 = 0
+        
+        h1, h2 = 0, 0
 
         for n in nums:
             tmp = max(h1 + n, h2)
             h1 = h2
             h2 = tmp
         return h2
-
-        
