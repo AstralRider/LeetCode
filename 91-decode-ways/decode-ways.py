6ec40b7a544f7430f89561f3a4e6eb68e7e946b5
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-06-01 15:52:11 +1000

    Time: 30 ms (90.35%) | Memory: 16.5 MB (89.87%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -6,13 +6,12 @@ class Solution:
         for i in range(len(s) - 1, -1, -1):
             if s[i] == '0':
                 cache[i] = 0
-                continue
+            else:
+                count = cache[i + 1]
 
-            count = cache[i + 1]
-
-            if (i + 1 < len(s) and (s[i] == '1' or (s[i] == '2' and s[i + 1] in '0123456'))):
-                count += cache[i + 2]
-            cache[i] = count
+                if (i + 1 < len(s) and (s[i] == '1' or (s[i] == '2' and s[i + 1] in '0123456'))):
+                    count += cache[i + 2]
+                cache[i] = count
 
         return cache[0]
 
