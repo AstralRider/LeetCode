42db52f1e110265990117f3244254ac05ed5ad88
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-06-04 21:44:08 +1000

    Time: 471 ms (16.02%) | Memory: 29.5 MB (87.30%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -6,21 +6,12 @@ class Solution:
         l = 0
         for r in range(len(arr)):
             total += arr[r]
-
-            if (r-l+1) < k:
-                continue
-
+            
             if (r-l+1) > k:
                 total -= arr[l]
                 l += 1
             
-            if total / k >= threshold:
+            if total / k >= threshold and (r-l+1) == k:
                 counter += 1
 
         return counter
-
-        #total = 6
-        #window_size = (1), (2), (3), (4) (3), 
-        #total / k = 2
-        #[2,2,2,2,5,5,5,8]
-        #counter = 0
