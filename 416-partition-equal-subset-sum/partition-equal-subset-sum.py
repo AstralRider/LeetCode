3c5c1dba6bce9f2a51ca27b3000071aae81091cd
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-06-08 19:04:07 +1000

    Time: 4945 ms (5.00%) | Memory: 206 MB (10.40%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -0,0 +1,37 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        
+        total = 0
+        for n in nums:
+            total += n
+        
+        
+        print(total/2)
+        if total % 2 != 0:
+            return False
+
+        target = total / 2
+        memo = {}
+        def dfs(i, currSum, target, nums, memo):
+            if i == len(nums):
+                return False
+            if (i, currSum) in memo:
+                return memo[(i, currSum)]
+            if currSum == target:
+                return True
+            
+            skip = dfs(i + 1, currSum, target, nums,memo)
+            include = False
+            if nums[i] + currSum <= target:
+                include = dfs(i + 1, nums[i] + currSum, target, nums, memo)
+            
+            res = skip or include
+            memo[(i, currSum)] = res
+            return memo[(i, currSum)]
+   
+        
+        return dfs(0, 0, target, nums,memo)
+
+
+            
+                
