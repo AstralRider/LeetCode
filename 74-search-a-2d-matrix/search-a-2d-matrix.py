915a531d2f48e9688a42fb1adaa6f3ebd58f53d3
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-07-16 13:42:40 +0530

    Time: 45 ms (60.70%) | Memory: 17.1 MB (16.55%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -5,31 +5,20 @@ class Solution:
 
         while top <= bottom:
             mid = (top + bottom) // 2
-            if target > matrix[mid][-1]:
-                top = mid + 1
-            elif target < matrix[mid][0]:
+            if target < matrix[mid][0]:
                 bottom = mid - 1
+            elif target > matrix[mid][-1]:
+                top = mid + 1
             else:
-                break
-
-
-        l = 0
-        r = len(matrix[mid]) - 1
-        arr = matrix[mid]
-
-        while l <= r:
-            m = (l + r) // 2
-            if target < arr[m]:
-                r = m - 1
-            elif target > arr[m]:
-                l = m + 1
-            else:
-                return True
-        return False
-        
-
-
-
-
-
-
+                l = 0 
+                r = len(matrix[mid])-1
+                while l <= r:
+                    m = (l + r) // 2
+                    if target < matrix[mid][m]:
+                        r = m - 1
+                    elif target > matrix[mid][m]:
+                        l = m + 1
+                    else:
+                        return True
+                return False
+            
