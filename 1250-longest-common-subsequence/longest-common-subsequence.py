5d610e398610e49b201f9b80cb641eee47625c65
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-07-27 12:38:50 +1000

    Time: 714 ms (25.45%) | Memory: 25 MB (87.54%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -4,17 +4,17 @@ class Solution:
         cache = [[-1] * len(text2) for _ in range(len(text1))]
         
         
-        def helper(text1, text2, i1, i2):
+        def helper(text1, text2, i1, i2, cache):
             if i1 == len(text1) or i2 == len(text2):
                 return 0
             if cache[i1][i2] != -1:
                 return cache[i1][i2]
             
             if text1[i1] == text2[i2]:
-                cache[i1][i2] = 1 + helper(text1, text2, i1 + 1, i2+1)
+                cache[i1][i2] = 1 + helper(text1, text2, i1 + 1, i2+1, cache)
                 return cache[i1][i2]
             else:
-                cache[i1][i2] = max(helper(text1, text2, i1 + 1, i2), helper(text1, text2, i1, i2 + 1))
+                cache[i1][i2] = max(helper(text1, text2, i1 + 1, i2, cache), helper(text1, text2, i1, i2 + 1, cache))
                 return cache[i1][i2]
         
-        return helper(text1, text2, 0, 0)
+        return helper(text1, text2, 0, 0, cache)
