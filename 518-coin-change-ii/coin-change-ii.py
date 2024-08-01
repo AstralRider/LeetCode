f57da3744ebc4baeda2989683c8b4b2f687e9b13
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-08-01 22:32:09 +1000

    Time: 2641 ms (5.01%) | Memory: 57.7 MB (24.04%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -5,21 +5,22 @@ class Solution:
 
         def dfs(i, total):
 
+            if total == amount:
+                return 1
+
             if i == len(coins):
                 return 0
             
-            if total > amount:
-                return 0
+            
             
             if (i, total) in cache:
                 return cache[i, total]
-            
-            if total == amount:
-                return 1
-            
+
             cache[i, total] = 0
             for j in range(i, len(coins)):
-                cache[i, total] += dfs(j, total + coins[j]) 
+                if total + coins[j] <= amount:
+                    cache[i, total] += dfs(j, total + coins[j]) 
+                
             
             return cache[i, total]
         return dfs(0, 0)
