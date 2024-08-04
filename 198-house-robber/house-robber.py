1c4911467e1d267a3fb0098f0d6fafbcfb8e0c7c
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-08-04 12:17:50 +1000

    Time: 30 ms (89.75%) | Memory: 16.6 MB (24.59%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,16 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-
-        cache = {}
         
-        def dfs(i, cache):
+        cache = {}
+
+        def dfs(i):
+            if i in cache:
+                return cache[i]
+
             if i >= len(nums):
                 return 0
             
-            if i in cache:
-                return cache[i]
-            
-            cache[i] = max(nums[i] + dfs(i+2, cache), dfs(i+1, cache))
-            return cache[i]
+            rob = dfs(i + 2)
+            skip = dfs(i + 1)
 
-        return dfs(0, cache)
+            cache[i] = max(rob + nums[i], skip)
+            return cache[i]
+        
+        return dfs(0)
