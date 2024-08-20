16c514bfce7f633c2f8a619eb2b6961c0f0496e2
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-08-20 20:54:50 +1000

    Time: 94 ms (96.00%) | Memory: 17.7 MB (40.87%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,21 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         
-        cache = [float('inf')] * len(nums)
+        l = 0 
+        r = 0
+        jumps = 0
 
-        def dfs(i):
-            if i >= len(nums) - 1:
-                return 0
-            
-            if cache[i] != float('inf'):
-                return cache[i]
-            
-            
+        while r < (len(nums) - 1):
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            jumps += 1
 
-            for j in range(1, nums[i] + 1):
-               cache[i] = min(dfs(i + j) + 1, cache[i])
-            
-            return cache[i]
         
-        return dfs(0)
-        
+        return jumps
