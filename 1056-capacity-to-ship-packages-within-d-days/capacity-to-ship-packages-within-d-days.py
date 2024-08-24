f1f75c15d928adb22c6a35d037854dd26c4d3b3b
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-08-24 10:54:07 +1000

    Time: 403 ms (49.76%) | Memory: 22.2 MB (74.01%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,35 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        
+        #minimum weight has to be max(weights)
+        #max weight has to be sum(weights) in case we need to ship everything in a single day
+
+        l = max(weights)
+        r = sum(weights)
+        res = float("inf")
+
+        def canShip(capacity):
+            days_required = 1
+            total = 0
+
+            for weight in weights:
+                if total + weight <= capacity:
+                    total += weight
+                else:
+                    total = weight
+                    days_required += 1
+            if days_required <= days:
+                return True
+            else:
+                return False
+            
+
+        while l <= r:
+            mid = (l + r) // 2
+            if canShip(mid):
+                res = min(res, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+        return res 
+
