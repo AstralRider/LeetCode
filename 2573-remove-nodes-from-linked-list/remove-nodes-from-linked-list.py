7b74c1809f408798586a188449f2ebd4ea94b648
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-08-28 19:21:12 +1000

    Time: 390 ms (88.29%) | Memory: 51.3 MB (59.58%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        #reverse linked list
+        #two pointers and find nodes smaller
+
+        curr = head
+        prev = None
+
+        while curr:
+            fast = curr.next
+            curr.next = prev
+            prev = curr
+            curr = fast
+        
+        p1 = prev
+        p2 = prev.next
+
+        while p2:
+            if p2.val < p1.val:
+                p2 = p2.next
+            else:
+                p1.next = p2
+                p1 = p2
+                p2 = p2.next
+        p1.next = p2
+        
+        new_curr = prev
+        new_prev = None
+
+        while new_curr:
+            new_fast = new_curr.next
+            new_curr.next = new_prev
+            new_prev = new_curr
+            new_curr = new_fast
+
+        return new_prev
+        
+        #reverse again and run
+
+
+        #[8,3,13,2,5]
+
