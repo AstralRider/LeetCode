174fb5eadd1cc796000342c9132cd426b29846df
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-08-30 08:43:24 +1000

    Time: 48 ms (54.51%) | Memory: 19.2 MB (78.90%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -11,12 +11,14 @@ class Solution:
         return self.diameter
     
     def dfs(self, root):
-      if not root:
-        return 0
-      
-      left = self.dfs(root.left)
-      right = self.dfs(root.right)
+        if not root:
+            return 0
+        
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
 
-      self.diameter = max(self.diameter, left + right)
+        self.diameter = max(self.diameter, left + right)
 
-      return 1 + max(left, right)
+        return max(left, right) + 1
+    
+    
