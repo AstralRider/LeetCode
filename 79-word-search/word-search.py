e7a5a30bc07d77b3bce24083957716a4e8b2ca8f
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-09-08 16:25:13 +1000

    Time: 4256 ms (46.74%) | Memory: 16.6 MB (69.54%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,38 +1,31 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        self.word = word
-        self.ROWS = len(board)
-        self.COLS = len(board[0])
-        for r in range(self.ROWS):
-          for c in range(self.COLS):
-            if self.backtrack(0, r, c, board, []):
-              return True
-        return False
+        ROWS = len(board)
+        COLS = len(board[0])
 
-    def backtrack(self, index, r, c, board, search):
-      if index == len(self.word):
-        return True
-      if r < 0 or r == self.ROWS:
+        def dfs(r, c, string, visited, index):
+            if index == len(word):
+                return True
+            if r == ROWS or r < 0 or c == COLS or c < 0:
+                return False
+            if (r,c) in visited:
+                return False
+            # string comparisons are O(N)
+            if word[index] != board[r][c]:
+                return False
+            
+            visited.add((r,c))
+            res = (
+                dfs(r + 1, c, string, visited, index + 1)
+                or dfs(r - 1, c, string, visited, index + 1)
+                or dfs(r, c + 1, string, visited, index + 1)
+                or dfs(r, c - 1, string, visited, index + 1)
+            )
+            visited.remove((r,c))
+            return res
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, "", set(), 0):
+                    return True
         return False
-      if c < 0 or c == self.COLS:
-        return False
-      if (r,c) in search:
-        return False
-      if self.word[index] != board[r][c]:
-        return False
-      
-      search = search[:]
-      search.append((r, c))
-      
-      return (
-      self.backtrack(index + 1, r + 1, c, board, search) or
-      self.backtrack(index + 1, r - 1, c, board, search) or
-      self.backtrack(index + 1, r, c + 1, board, search) or
-      self.backtrack(index + 1, r, c - 1, board, search)
-      )
-
-
-
-
-      
-      
