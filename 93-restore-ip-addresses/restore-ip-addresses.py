d8e4b17ad203153e831874f111a2ce360b12f358
Astral Rider <100898882+AstralRider@users.noreply.github.com>
2024-09-10 09:04:35 +1000

    Time: 69 ms (5.97%) | Memory: 16.5 MB (60.78%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.py
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -1,28 +1,25 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
+        #integer < 256
+        #integer doesn't start with 0
+
         res = []
 
-        def dfs(i, dots, string):
-            print(string)
-            if i >= len(s) and dots == 4:
-                res.append(string[:-1])
-                return
-            if dots == 4:
+        def backtrack(i, current, dots):
+            if i >= len(s) and dots == 0:
+                res.append(current[:-1])
                 return
             
-            for j in range(i,  len(s)):
-                
-                digits = s[i: j+1]
-
-                if len(digits) > 1 and digits[0] == '0':
-                    return False
-
-                if int(digits) > 255:
-                    return False
-                
-                dfs(j+1, dots +1 , string + digits + ".")
-
-        dfs(0, 0, "")
+            for j in range(i, i + 3):
+                if j == len(s):
+                    return
+                if int(s[i: j + 1]) < 256:
+                    if len(s[i: j + 1]) > 1 and s[i] != '0':
+                        backtrack(j + 1, current + s[i: j + 1] + '.', dots - 1)
+                    
+                    if len(s[i: j + 1]) == 1:
+                        backtrack(j + 1, current + s[i: j + 1] + '.', dots - 1)
+        
+        backtrack(0, "", 4)
         return res
-
-
+                    
